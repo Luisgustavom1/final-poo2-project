73d50fe2400b62277abcaa4a203f957ce94998b7
r1beirin <mateusriibeiro@gmail.com>
2023-05-23 11:00:19 -0300

    new galinha.png and init logically part galinha in screen

--- core/src/com/freewaygpt/game/FreewayGPT.java
+++ core/src/com/freewaygpt/game/FreewayGPT.java
@@ -3,21 +3,34 @@ package com.freewaygpt.game;
 import java.util.HashMap;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.freewaygpt.game.design.Colors;
+import com.freewaygpt.game.elements.Assets;
 import com.freewaygpt.game.elements.EndPoint;
 import com.freewaygpt.game.elements.InitialPoint;
 import com.freewaygpt.game.entity.Sidewalk;
 
 public class FreewayGPT extends ApplicationAdapter {
 	private Colors colors;
+	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	private HashMap<String, Sidewalk> sidewalks = new HashMap<String, Sidewalk>();
+	private Texture chickenImage;
+	private Rectangle chicken;
 
 	@Override
 	public void create() {
 		colors = new Colors();
+		chickenImage = new Assets().chickenImageCreate();
+		chicken = new Assets().chickenCreate();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 1024, 520);
+		batch = new SpriteBatch();
 
 		sidewalks.put("init", new InitialPoint());
 		sidewalks.put("end", new EndPoint());
@@ -29,14 +42,28 @@ public class FreewayGPT extends ApplicationAdapter {
 
 		sidewalks.get("init").render();
 		sidewalks.get("end").render();
+
+		// tell the camera to update its matrices.
+		camera.update();
+
+		// tell the SpriteBatch to render in the
+		// coordinate system specified by the camera.
+		batch.setProjectionMatrix(camera.combined);
+
+		// begin a new batch and draw the chicken and
+		batch.begin();
+		batch.draw(chickenImage, chicken.x, chicken.y);
+		batch.end();
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
+		chickenImage.dispose();
 
 		for (Sidewalk sidewalk:sidewalks.values()) {
 			sidewalk.dispose();
 		}
+
+		batch.dispose();
 	}
 }
