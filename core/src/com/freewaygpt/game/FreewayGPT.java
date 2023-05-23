83924441146d091e721e2a1fa30a16de67634fec
r1beirin <mateusriibeiro@gmail.com>
2023-05-23 11:32:58 -0300

    fix limit screen axis y user input

--- core/src/com/freewaygpt/game/FreewayGPT.java
+++ core/src/com/freewaygpt/game/FreewayGPT.java
@@ -3,10 +3,13 @@ package com.freewaygpt.game;
 import java.util.HashMap;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.freewaygpt.game.design.Colors;
 import com.freewaygpt.game.elements.Assets;
@@ -54,6 +57,20 @@ public class FreewayGPT extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(chickenImage, chicken.x, chicken.y);
 		batch.end();
+
+		// process user input
+		if(Gdx.input.isTouched()) {
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			chicken.y = 20;
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) chicken.y -= 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)) chicken.y += 200 * Gdx.graphics.getDeltaTime();
+
+		// make sure the chicken stays within the screen bounds
+		if(chicken.y < 0) chicken.y = 0;
+		if(chicken.y > 436) chicken.y = 436;
 	}
 
 	@Override
